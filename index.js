f33a1aa35062a61bc4d51e51a2842e5deb9afb91
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-10-15 22:58:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    const win=record.find((record) => record.result === "W")
+    if (win === undefined) {
+        return undefined;
+      }
+     if (win !== undefined)
+      return win.year;
+    }
